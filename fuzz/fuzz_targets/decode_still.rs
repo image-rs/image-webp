039a794961cb60fbc32e38fd1ca30ee836595aca
Jonathan Behrens <fintelia@gmail.com>
2023-12-25 18:54:16 -0800

    Limit max memory allocation by fuzz target

--- fuzz/fuzz_targets/decode_still.rs
+++ fuzz/fuzz_targets/decode_still.rs
@@ -8,7 +8,10 @@ fuzz_target!(|input: &[u8]| {
     if let Ok(mut decoder) = decoder {
         let (width, height) = decoder.dimensions();
         let bytes_per_pixel = if decoder.has_alpha() { 4 } else { 3 };
-        let mut data = vec![0; width as usize * height as usize * bytes_per_pixel];
-        let _ = decoder.read_image(&mut data);
+        let total_bytes = width as usize * height as usize * bytes_per_pixel;
+        if total_bytes <= 1024 * 1024 * 1024 {
+            let mut data = vec![0; total_bytes];
+            let _ = decoder.read_image(&mut data);
+        }
     }
 });
