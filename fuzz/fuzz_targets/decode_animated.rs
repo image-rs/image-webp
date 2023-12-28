67a0f4eb181525f1cccbf0a73c58cd41977f74cf
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2023-12-28 00:19:42 +0000

    Initial fuzzing harness for animated images (#32)

---
+++ fuzz/fuzz_targets/decode_animated.rs
@@ -0,0 +1,19 @@
+#![no_main]
+
+use libfuzzer_sys::fuzz_target;
+use std::io::Cursor;
+
+fuzz_target!(|input: &[u8]| {
+    let decoder = webp::WebPDecoder::new(Cursor::new(input));
+    if let Ok(mut decoder) = decoder {
+        let (width, height) = decoder.dimensions();
+        let bytes_per_pixel = if decoder.has_alpha() { 4 } else { 3 };
+        let total_bytes = width as usize * height as usize * bytes_per_pixel;
+        if total_bytes <= 1024 * 1024 * 1024 {
+            if decoder.has_animation() {
+                let mut data = vec![0; total_bytes];
+                while let Ok(_delay) = decoder.read_frame(&mut data) {};
+            }
+        }
+    }
+});
