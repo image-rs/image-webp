68ea852a596c99dfbb8f8314b59cfc5bf21c31af
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2024-12-18 20:54:05 +0000

    Replace libwebp routines with my own, and wow, they pass the tests with better tolerances than libwebp blending!

--- src/alpha_blending.rs
+++ src/alpha_blending.rs
@@ -1,58 +1,58 @@
-//! Optimized alpha blending routines based on libwebp
-//!
-//! https://github.com/webmproject/libwebp/blob/e4f7a9f0c7c9fbfae1568bc7fa5c94b989b50872/src/demux/anim_decode.c#L215-L267
+//! Optimized alpha blending routines
 
-const fn channel_shift(i: u32) -> u32 {
-    i * 8
+/// Divides by 255, rounding to nearest (as opposed to down, like regular integer division does).
+/// TODO: cannot output 256, so the output is effecitively u8. Plumb that through the code.
+//
+// Sources:
+// https://arxiv.org/pdf/2202.02864
+// https://github.com/image-rs/image-webp/issues/119#issuecomment-2544007820
+#[inline]
+fn div_by_255(v: u16) -> u16 {
+    ((((v + 0x80) >> 8) + v + 0x80) >> 8).min(255) // TODO: .min(255) is almost certainly unnecessary
 }
 
-/// Blend a single channel of `src` over `dst`, given their alpha channel values.
-/// `src` and `dst` are assumed to be NOT pre-multiplied by alpha.
-fn blend_channel_nonpremult(
-    src: u32,
-    src_a: u8,
-    dst: u32,
-    dst_a: u8,
-    scale: u32,
-    shift: u32,
-) -> u8 {
-    let src_channel = ((src >> shift) & 0xff) as u8;
-    let dst_channel = ((dst >> shift) & 0xff) as u8;
-    let blend_unscaled = (src_channel as u32 * src_a as u32) + (dst_channel as u32 * dst_a as u32);
-    // debug_assert!(blend_unscaled < (1u64 << 32) as u32 / scale); // TODO: fails for some reason?
-    ((blend_unscaled * scale) >> channel_shift(3)) as u8
-}
-
-/// Blend `src` over `dst` assuming they are NOT pre-multiplied by alpha.
-fn blend_pixel_nonpremult(src: u32, dst: u32) -> u32 {
-    let src_a = ((src >> channel_shift(3)) & 0xff) as u8;
-
-    if src_a == 0 {
-        dst
-    } else {
-        let dst_a = ((dst >> channel_shift(3)) & 0xff) as u8;
-        // Approximate integer arithmetic for: dst_factor_a = (dst_a * (255 - src_a)) / 255
-        let dst_factor_a = (dst_a as u32 * (256 - src_a as u32)) >> 8;
-        let blend_a = src_a as u32 + dst_factor_a as u32;
-        let scale = (1u32 << 24) / blend_a;
-
-        let blend_r =
-            blend_channel_nonpremult(src, src_a, dst, dst_factor_a as u8, scale, channel_shift(0));
-        let blend_g =
-            blend_channel_nonpremult(src, src_a, dst, dst_factor_a as u8, scale, channel_shift(1));
-        let blend_b =
-            blend_channel_nonpremult(src, src_a, dst, dst_factor_a as u8, scale, channel_shift(2));
-        assert!(src_a as u32 + dst_factor_a < 256);
-
-        ((blend_r as u32) << channel_shift(0))
-            | ((blend_g as u32) << channel_shift(1))
-            | ((blend_b as u32) << channel_shift(2))
-            | ((blend_a as u32) << channel_shift(3))
-    }
+/// Rounding integer division (as opposed to truncating)
+///
+/// Works for the range of values we care about,
+/// which is verified by tests against f64.round()
+///
+/// Breaks down when b is u16 and large due to overflow, but we don't need that!
+fn rounding_div(a: u16, b: u8) -> u16 {
+    let b_u16 = u16::from(b);
+    (a + (b_u16 / 2)) / b_u16
 }
 
 pub(crate) fn do_alpha_blending(buffer: [u8; 4], canvas: [u8; 4]) -> [u8; 4] {
-    blend_pixel_nonpremult(u32::from_le_bytes(buffer), u32::from_le_bytes(canvas)).to_le_bytes()
+    let canvas_alpha = u16::from(canvas[3]);
+    let buffer_alpha = u16::from(buffer[3]);
+    // repeating calculation extracted from two places and computed once. TODO: better name
+    let shared_term = canvas_alpha - div_by_255(canvas_alpha * buffer_alpha);
+    let blend_alpha = (buffer_alpha + shared_term) as u8;
+
+    let blend_rgb: [u8; 3] = if blend_alpha == 0 {
+        [0, 0, 0] // TODO: do we really need this special case?
+    } else {
+        let mut rgb = [0u8; 3];
+        for i in 0..3 {
+            let canvas_u16 = u16::from(canvas[i]);
+            let buffer_u16 = u16::from(buffer[i]);
+
+            // We split this arithmetic over two calls to rounding_div
+            // so that the result does not overflow at any point.
+            // All the values involved are 255 or lower, and 255*255 is 65,025
+            // which leaves room to spare in u16 capacity of 65,535.
+            // In fact, just enough room for our rounding integer division hack!
+            let val = rounding_div(buffer_u16 * buffer_alpha, blend_alpha)
+                + rounding_div(canvas_u16 * shared_term, blend_alpha);
+
+            // we only need u16 for intermediate calculations, convert output back to u8
+            rgb[i] = val as u8;
+        }
+
+        rgb
+    };
+
+    [blend_rgb[0], blend_rgb[1], blend_rgb[2], blend_alpha]
 }
 
 #[cfg(test)]
