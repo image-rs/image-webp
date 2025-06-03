e9a3fb462329f16d217793187e9b4778e1722c5d
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2025-06-03 10:10:40 +0100

    Remove bounds checks from color transform hot loop (#133)

--- src/lossless_transform.rs
+++ src/lossless_transform.rs
@@ -350,11 +350,19 @@ pub(crate) fn apply_color_transform(
     let width = usize::from(width);
 
     for (y, row) in image_data.chunks_exact_mut(width * 4).enumerate() {
-        for (block_x, block) in row.chunks_mut(4 << size_bits).enumerate() {
-            let block_index = (y >> size_bits) * block_xsize + block_x;
-            let red_to_blue = transform_data[block_index * 4];
-            let green_to_blue = transform_data[block_index * 4 + 1];
-            let green_to_red = transform_data[block_index * 4 + 2];
+        let row_transform_data_start = (y >> size_bits) * block_xsize * 4;
+        // the length of block_tf_data should be `block_xsize * 4`, so we could slice it with [..block_xsize * 4]
+        // but there is no point - `.zip()` runs until either of the iterators is consumed,
+        // so the extra slicing operation would be doing more work for no reason
+        let row_tf_data = &transform_data[row_transform_data_start..];
+
+        for (block, transform) in row
+            .chunks_mut(4 << size_bits)
+            .zip(row_tf_data.chunks_exact(4))
+        {
+            let red_to_blue = transform[0];
+            let green_to_blue = transform[1];
+            let green_to_red = transform[2];
 
             for pixel in block.chunks_exact_mut(4) {
                 let green = u32::from(pixel[1]);
